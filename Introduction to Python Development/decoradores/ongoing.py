4ffdeb610d79611b12dfd9f5c323cb34f2772b07
adritrix <adrinaerf.n@gmail.com>
2022-10-31 17:22:19 +0100

    hasta el captulo 11 modified

--- Introduction to Python Development/decoradores/ongoing.py
+++ Introduction to Python Development/decoradores/ongoing.py
@@ -1,8 +1,17 @@
 def inspect(func, *args):
-    print(f"Running {func.__name__}")
-    val = func(*args)
-    print(val)
-    return val
+    def warpped_func(*args, **kwargs):
+        print(f"Running {func.__name__}")
+        val = func(*args)
+        print(val)
+        return val
+    return warpped_func
 
+
+@inspect
 def combine(a, b):
     return a + b
+
+# class User:
+#     base_url = 'https://example.com/api'
+
+#     def__init__(self, first_name)
