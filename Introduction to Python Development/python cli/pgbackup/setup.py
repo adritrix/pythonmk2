d23e1c57afb65cf803b7b7f25147e9f3a6ae0bd3
adritrix <adrianerf.n@gmail.com>
2022-11-04 12:11:55 +0100

    Project set un completed

--- Introduction to Python Development/python cli/pgbackup/setup.py
+++ Introduction to Python Development/python cli/pgbackup/setup.py
@@ -5,5 +5,13 @@ with open('README.md','r') as f:
 
 
 setup(
-    name = 'pgbackup' version = '0.1.0'
+    name='pgpackup',
+    version='0.1.0',
+    author='Keith Thompson',
+    author_email='keith@linuxacademy.com',
+    description='A utility for backing up PostgreSQL databases',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/your_username/pgbackup',
+    packages=find_packages('src')
 )
